e9e69b754830f1ee9a6f12c51e1ebfb11af86963
Jenise Ruehle <jela07@gmail.com>
2020-09-01 19:40:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
   
-  @@item = []
+  @@items = []
   
   def call(env)
     resp = Rack::Response.new 
@@ -8,7 +8,7 @@ class Application
   
     if req.path.match(/items/)
       item_name = req.path.split("/items").last 
-      item = @@item.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
       
       if item.nil?
         resp.write "Item not found"
