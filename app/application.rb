d14f561c166702b094116ccf06cad214b0246122
Jenise Ruehle <jela07@gmail.com>
2020-09-01 19:35:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       
       if item.nil?
         resp.write "Item not found"
-        resp.status = 404
+        resp.status = 400
       else 
       resp.write item.price
     end
