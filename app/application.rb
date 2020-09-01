5694f2f84fe0da739ec0fd855e5af67a5546dba0
Jenise Ruehle <jela07@gmail.com>
2020-09-01 19:38:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,13 +15,12 @@ class Application
         resp.status = 400
       else 
       resp.write item.price
-    end
-  end 
-end 
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end 
   
-  
-  
-  
-  
-  
+    resp.finish
+  end
 end 
