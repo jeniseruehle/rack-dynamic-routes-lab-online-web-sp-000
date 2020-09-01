d7997d616c8a50416f4a75ed65245536b813d796
Jenise Ruehle <jela07@gmail.com>
2020-09-01 19:31:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,8 @@ class Application
     req = Rack::Request.new(env)
   
     if req.path.match(/items/)
+      item_name = req.path.split("/items").last 
+      item = @@items.find{|i| i.price == item_name}
       
   end 
   
